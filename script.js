ffc906d5cb0d6805501ea92fa627a227dec731c0
markpino <markpino1998@outlook.com>
2024-03-18 18:37:26 -0600

    Primeros pasos en JS, se declaro , , array  y la funcion

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const textUserIn = document.querySelector("#input-text");
+const textCryptOut = document.querySelector("#output-text");
 
+const cryptMap = [
+    ["a", "ai"],      //0
+    ["e", "entre"],   //1
+    ["i", "imas"],    //2
+    ["o", "ober"],    //3
+    ["u", "ufat"],    //4
+    ];
 
-function encrypt(){
-    var inputText = document.getElementById( "input-text" ).value;
-}
+    function  btnEncryptText() {
+        const  textEncrypted = textUserIn.value;
+        console.log(textEncrypted);
+    }
+
